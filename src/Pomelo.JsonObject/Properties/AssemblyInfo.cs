c0e3d8f0a7c93aebca3f1f68c736fdfa88095c07
Alexander van der Kolk <avdk@soltegro.nl>
2019-07-16 11:13:20 +0200

    Signed Assembly.

--- src/Pomelo.JsonObject/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) Pomelo Foundation. All rights reserved.
-// Licensed under the MIT. See LICENSE in the project root for license information.
-
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Pomelo.JsonObject.Tests")]
